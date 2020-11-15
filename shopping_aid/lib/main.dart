0f5d140a08ab2875d168eafcd2d802df408d3174
Gilles Van Bievrliet <glsvbv@gmail.com>
2020-11-15 02:22:59 +0100

    made bottom floating navigation + floating button to add a product

--- shopping_aid/lib/main.dart
+++ shopping_aid/lib/main.dart
@@ -23,64 +23,119 @@ class MyApp extends StatelessWidget {
               ] 
             ),
           ),
-          backgroundColor: Colors.blueGrey[700],
+          backgroundColor: Colors.blueGrey[800],
           
         ),
-        body: 
+        body: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Container(
+                  child: ListView(
+                    children: <Widget> [
+                      Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Card(  
+                          color: Colors.blueGrey[900],
+                          child: ListTile(
+                            leading: FlutterLogo(size: 56.0),
+                            title: Text("Dit is lijst item", style: 
+                              TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                            subtitle: Text("2 pakken", style: 
+                              TextStyle(
+                                color: Colors.grey[400],
+                                fontWeight: FontWeight.normal
+                              ),
+                            ),
+                            trailing: 
+                                Wrap(
+                                  alignment: WrapAlignment.center,
+                                  spacing: 12,
+                                  children: <Widget>[
+                                    Text('€12,00', style: 
+                                      TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    Icon(Icons.delete_forever_rounded, color: Colors.red),    
+                                  ],
+                                ),    
+                            ) 
+                          )
+                         ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                    child: Container(
-                      child: ListView(
-                        children: <Widget> [
-                          Padding(
-                            padding: EdgeInsets.all(5.0),
-                            child: Card(  
-                              color: Colors.blueGrey[900],
-                              child: ListTile(
-                                leading: FlutterLogo(size: 56.0),
-                                title: Text("Dit is lijst item", style: 
-                                  TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold
-                                  ),
-                                ),
-                                subtitle: Text("2 pakken", style: 
-                                  TextStyle(
-                                    color: Colors.grey[400],
-                                    fontWeight: FontWeight.normal
-                                  ),
-                                ),
-                                trailing: 
-                                    Wrap(
-                                      alignment: WrapAlignment.center,
-                                      spacing: 12,
-                                      children: <Widget>[
-                                        Text('€12,00', style: 
-                                          TextStyle(
-                                            fontSize: 20.0,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold
-                                          ),
-                                        ),
-                                        Icon(Icons.delete_forever_rounded, color: Colors.red),    
-                                      ],
-                                    ),    
-                                ) 
-                              )
-                             ),
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.fromLTRB(0, 0, 10, 20) ,
+                    child: FloatingActionButton(
+                      child: Icon(Icons.local_activity),
+                      onPressed: null
+                    ),
+                  ),
+
+                  Container(
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                      child: BottomNavigationBar(
+                        unselectedItemColor: Colors.white,
+                        backgroundColor: Colors.blueGrey[700],
+                        showUnselectedLabels: true,
+                        type: BottomNavigationBarType.fixed,
+                        items: [
+                          BottomNavigationBarItem(
+                            backgroundColor: Colors.white,
+                            icon: Icon(Icons.local_activity,
+                              color: Colors.white
+                            ),
+                            label: "Aardappel",
+                          ),
+                          BottomNavigationBarItem(
+                            backgroundColor: Colors.white,
+                            icon: Icon(Icons.local_activity,
+                              color: Colors.white
+                            ),
+                            label: "Aardappel",
+                          ), 
+                          BottomNavigationBarItem(
+                            backgroundColor: Colors.white,
+                            icon: Icon(Icons.local_activity,
+                              color: Colors.white
+                            ),
+                            label: "Aardappel",
+                          ) ,
+                          BottomNavigationBarItem(
+                            backgroundColor: Colors.white,
+                            icon: Icon(Icons.local_activity,
+                              color: Colors.white
+                            ),
+                            label: "Aardappel",
+                          ) 
                         ],
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],  
           ),
+           
+        )  
     );
   }
 }
