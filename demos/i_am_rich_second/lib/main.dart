bbfba372484600cdc1cb3394e1d96dfd4993316f
Gilles Van Bievrliet <glsvbv@gmail.com>
2020-11-18 19:02:51 +0100

    Proberen het image probleem op te lossen

--- demos/i_am_rich_second/lib/main.dart
+++ demos/i_am_rich_second/lib/main.dart
@@ -4,140 +4,59 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  final src = 'https://picsum.photos/250?image=9';
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      title: 'Shoppina',
+      title: 'i am rich',
       home: Scaffold(
         backgroundColor: Colors.blueGrey[800],
         appBar: AppBar(
-          title: Padding(
-            padding: EdgeInsets.fromLTRB(5, 15, 5, 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Shoppina'),
-                Text('€12,34')
-              ] 
-            ),
+          title: Center(
+            child: Text('I Am Rich')
           ),
-          backgroundColor: Colors.blueGrey[800],
+          backgroundColor: Colors.blueGrey[900],
           
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Container(
-                  child: ListView(
-                    children: <Widget> [
-                      Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: Card(  
-                          color: Colors.blueGrey[900],
-                          child: ListTile(
-                            leading: FlutterLogo(size: 56.0),
-                            title: Text("Dit is lijst item", style: 
-                              TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            subtitle: Text("2 pakken", style: 
-                              TextStyle(
-                                color: Colors.grey[400],
-                                fontWeight: FontWeight.normal
-                              ),
-                            ),
-                            trailing: 
-                                Wrap(
-                                  alignment: WrapAlignment.center,
-                                  spacing: 12,
-                                  children: <Widget>[
-                                    Text('€12,00', style: 
-                                      TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold
-                                      ),
-                                    ),
-                                    Icon(Icons.delete_forever_rounded, color: Colors.red),    
-                                  ],
-                                ),    
-                            ) 
-                          )
-                         ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.fromLTRB(0, 0, 10, 20) ,
-                    child: FloatingActionButton(
-                      child: Icon(Icons.local_activity),
-                      onPressed: null
-                    ),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      child: BottomNavigationBar(
-                        unselectedItemColor: Colors.white,
-                        backgroundColor: Colors.blueGrey[700],
-                        showUnselectedLabels: true,
-                        type: BottomNavigationBarType.fixed,
-                        items: [
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ),
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ), 
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ) ,
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ) 
-                        ],
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/homer.png'),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text('Yoo',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white
+                        )
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],  
+                  Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Yoo',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white
+                        )
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ]
           ),
         )  
+      )
     );
   }
 }
-
 
 
