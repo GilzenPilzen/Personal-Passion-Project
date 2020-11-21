2c03c5cd652d847d84bbebcb3cd67c06e9c5d148
Gilles Van Bievrliet <glsvbv@gmail.com>
2020-11-21 16:04:44 +0100

    lichte code optimalisatie

--- shopping_aid/lib/main.dart
+++ shopping_aid/lib/main.dart
@@ -2,146 +2,154 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final src = 'https://picsum.photos/250?image=9';
+class MyApp extends StatelessWidget {
 
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Shoppina',
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[800],
-        appBar: AppBar(
-          title: Padding(
-            padding: EdgeInsets.fromLTRB(5, 15, 5, 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Shoppina'),
-                Text('€12,34')
-              ] 
+      home: ShoppingListView(context)
+    );
+  }
+}
+
+class ShoppingListView extends StatefulWidget {
+  ShoppingListView(BuildContext context);
+
+  @override
+  _ShoppingListViewState createState() => _ShoppingListViewState();
+}
+
+class _ShoppingListViewState extends State<ShoppingListView> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    backgroundColor: Colors.blueGrey[800],
+    appBar: AppBar(
+      title: Padding(
+        padding: EdgeInsets.fromLTRB(5, 15, 5, 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text('Shoppina'),
+            Text('€12,34')
+          ] 
+        ),
+      ),
+      backgroundColor: Colors.blueGrey[800],
+      
+    ),
+    body: Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Container(
+              child: ListView(
+                children: <Widget> [
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Card(  
+                      color: Colors.blueGrey[900],
+                      child: ListTile(
+                        leading: FlutterLogo(size: 56.0),
+                        title: Text("Dit is lijst item", style: 
+                          TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        subtitle: Text("2 pakken", style: 
+                          TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.normal
+                          ),
+                        ),
+                        trailing: 
+                            Wrap(
+                              alignment: WrapAlignment.center,
+                              spacing: 12,
+                              children: <Widget>[
+                                Text('€12,00', style: 
+                                  TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Icon(Icons.delete_forever_rounded, color: Colors.red),    
+                              ],
+                            ),    
+                        ) 
+                      )
+                      ),
+                ],
+              ),
             ),
           ),
-          backgroundColor: Colors.blueGrey[800],
-          
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Container(
-                  child: ListView(
-                    children: <Widget> [
-                      Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: Card(  
-                          color: Colors.blueGrey[900],
-                          child: ListTile(
-                            leading: FlutterLogo(size: 56.0),
-                            title: Text("Dit is lijst item", style: 
-                              TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            subtitle: Text("2 pakken", style: 
-                              TextStyle(
-                                color: Colors.grey[400],
-                                fontWeight: FontWeight.normal
-                              ),
-                            ),
-                            trailing: 
-                                Wrap(
-                                  alignment: WrapAlignment.center,
-                                  spacing: 12,
-                                  children: <Widget>[
-                                    Text('€12,00', style: 
-                                      TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold
-                                      ),
-                                    ),
-                                    Icon(Icons.delete_forever_rounded, color: Colors.red),    
-                                  ],
-                                ),    
-                            ) 
-                          )
-                         ),
+        Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.fromLTRB(0, 0, 10, 20) ,
+                child: Builder(
+                  builder: (context) => FloatingActionButton(
+                    child: Icon(Icons.local_activity),
+                    onPressed: (){
+                      _productsOverlay(context);
+                    }
+                  ),
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  child: BottomNavigationBar(
+                    unselectedItemColor: Colors.white,
+                    backgroundColor: Colors.blueGrey[700],
+                    showUnselectedLabels: true,
+                    type: BottomNavigationBarType.fixed,
+                    items: [
+                      BottomNavigationBarItem(
+                        backgroundColor: Colors.white,
+                        icon: Icon(Icons.local_activity,
+                          color: Colors.white
+                        ),
+                        label: "Aardappel",
+                      ),
+                      BottomNavigationBarItem(
+                        backgroundColor: Colors.white,
+                        icon: Icon(Icons.local_activity,
+                          color: Colors.white
+                        ),
+                        label: "Aardappel",
+                      ), 
+                      BottomNavigationBarItem(
+                        backgroundColor: Colors.white,
+                        icon: Icon(Icons.local_activity,
+                          color: Colors.white
+                        ),
+                        label: "Aardappel",
+                      ) ,
+                      BottomNavigationBarItem(
+                        backgroundColor: Colors.white,
+                        icon: Icon(Icons.local_activity,
+                          color: Colors.white
+                        ),
+                        label: "Aardappel",
+                      ) 
                     ],
                   ),
                 ),
               ),
-            ),
-            Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.fromLTRB(0, 0, 10, 20) ,
-                    child: Builder(
-                      builder: (context) => FloatingActionButton(
-                        child: Icon(Icons.local_activity),
-                        onPressed: (){
-                          _productsOverlay(context);
-                        }
-                      ),
-                    ),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      child: BottomNavigationBar(
-                        unselectedItemColor: Colors.white,
-                        backgroundColor: Colors.blueGrey[700],
-                        showUnselectedLabels: true,
-                        type: BottomNavigationBarType.fixed,
-                        items: [
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ),
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ), 
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ) ,
-                          BottomNavigationBarItem(
-                            backgroundColor: Colors.white,
-                            icon: Icon(Icons.local_activity,
-                              color: Colors.white
-                            ),
-                            label: "Aardappel",
-                          ) 
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],  
+            ],
           ),
-        )  
+        ],  
+      ),
     );
   }
 }
@@ -154,7 +162,7 @@ void _productsOverlay(context) {
     backgroundColor: Colors.grey[800],
     builder: (BuildContext bc) {
     
-    return (
+    return
       Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.all(15),
@@ -181,20 +189,19 @@ void _productsOverlay(context) {
                   child: Column(
                     mainAxisSize: MainAxisSize.max,
                     children: [
-                      _productQuickView()
+                      _ProductQuickView(),
                     ],
                   )
                 )
               ]
           )
         ),
-      )
-    );
-  });
+      );
+    }
+  );
 }
 
-// ignore: camel_case_types
-class _productQuickView extends StatelessWidget {
+class _ProductQuickView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -209,7 +216,7 @@ class _productQuickView extends StatelessWidget {
               padding: EdgeInsets.all(15),
               margin: EdgeInsets.only(bottom: 35),
               child: Image(
-                width: 300,
+                width: 250,
                 image: AssetImage("assets/images/cola.jpg"),
               )
             ),
@@ -223,5 +230,11 @@ class _productQuickView extends StatelessWidget {
   }
 }
 
-
-
+class AllProducts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
