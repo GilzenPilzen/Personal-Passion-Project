39e243b59f59e566e42ebea6adbc559d7809f6df
Gilles Van Bievrliet <glsvbv@gmail.com>
2020-12-01 20:59:28 +0100

    augmented reality demo is made -> not tested on iOS

---
+++ demos/augmented_reality/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+// import 'package:vector_math/vector_math_64.dart' as vector;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ArCoreController arCoreController;
+
+  _onArCoreViewCreated(ArCoreController _arcoreController) {
+    arCoreController = _arcoreController;
+    _addSphere(arCoreController);
+  }
+
+  _addSphere(ArCoreController _arcoreController){
+      final material = ArCoreMaterial(color: Colors.blue);
+      final sphere = ArCoreSphere(materials:  [material], radius: 0.2);
+      final node = ArCoreNode(
+        shape: sphere, 
+        // position: vector.Vector3(
+        //   0, 0, -1
+        // )
+      );
+
+      _arcoreController.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'i am rich',
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[800],
+        body: ArCoreView(onArCoreViewCreated: _onArCoreViewCreated,)
+
+      )
+    );
+  }
+}
