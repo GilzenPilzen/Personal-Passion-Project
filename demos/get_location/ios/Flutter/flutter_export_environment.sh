fbbb49b0cc21b4ef12bcdedae624c631a012c6f1
GilzenPilzen <gilles.van.biervliet@student.howest.be>
2020-12-10 17:26:27 +0100

    een hoop dingen om het te laten werken op een ios device -> AR Kit nog niet gecode

---
+++ demos/get_location/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/glsvbv/developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/glsvbv/Documents/My HDD/C-Applications/MAMP/www/school/DEVINE 3/PersonalPassionProject/Repo/Personal-Passion-Project/demos/get_location"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/glsvbv/developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
