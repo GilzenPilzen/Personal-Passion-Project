6b795e46f4d6e8ab9c932f176506f2419f328c8f
GilzenPilzen <gilles.van.biervliet@student.howest.be>
2020-12-13 19:50:41 +0100

    splashscreen toegevoegd

--- shopping_aid/lib/main.dart
+++ shopping_aid/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'views/shoppinglist.dart';
 import 'views/homepage.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 // HEAD FUNCTION
 void main() => runApp(MyApp());
@@ -10,7 +11,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shoppina',
-      home: HomePageView()
+      home:
+      SplashScreen(
+        seconds: 5,
+        navigateAfterSeconds: HomePageView(),
+        title: Text('Welcome to Shoppina',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[800],
+        styleTextUnderTheLoader: new TextStyle(),
+        onClick: ()=>print("Flutter Egypt"),
+        loaderColor: Colors.transparent
+      )
+      
+      
     );
   }
 }
