5a842ce2c94bd47ce57918a7aec8b11c66ea0439
GilzenPilzen <gilles.van.biervliet@student.howest.be>
2020-12-15 02:37:11 +0100

    locatie wordt nu live upgedate + er wordt weergegeven hoe ver de persoon van zijn doel zit

--- shopping_aid/lib/views/navigator_android.dart
+++ shopping_aid/lib/views/navigator_android.dart
@@ -11,14 +11,6 @@ import 'package:geomag/geomag.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:location/location.dart' hide LocationAccuracy;
 
-// class ArCoreNavigator extends StatefulWidget {
-//   // List data = [];
-//   // ArCoreNavigator({this.data});
-//   @override
-//   _ArCoreNavigatorState createState() => _ArCoreNavigatorState();
-//   // ArCoreNavigator({List this.data});
-  
-// }
 // ignore: must_be_immutable
 class ArCoreNavigator extends StatefulWidget {
 
@@ -46,7 +38,8 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
   var longitude;
   var latitudeDouble;
   var longitudeDouble;
-  double distanceToGoal;
+  var distance = "nog aan het berekenen";
+  bool visible = false;
   StreamSubscription _locationSubscription;
   Location _locationTracker = new Location();
 
@@ -58,47 +51,54 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
   }
 
   Future<double> _getDeclination(latitude, longitude, altitude) async {
+
     // ignore: await_only_futures
     final result = await GeoMag().calculate(
       latitude, longitude, altitude * 3.28084
     ); // m -> ft
     return result.dec;
-    // ignore: dead_code
-  }
-
-  void _getCurrentLocation() async {
-    var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-
-
-    // print(position);
-    setState(() {
-      latitudeDouble = double.parse(data['latitude']);
-      longitudeDouble = double.parse(data['longitude']);
-      print(latitudeDouble);
-      print(longitudeDouble);
-      bearing = Geolocator.bearingBetween(position.latitude, position.longitude, latitudeDouble, longitudeDouble);
-      print(bearing);  
-      cords= position;
-      print(cords);
-      _getDeclination(position.latitude, position.longitude, position.latitude).then((dec) {
-        setState(() {
-          _declination = dec;
-        });
-      });
-    });
   }
 
   void getLiveLocation() {
 
     if (_locationSubscription != null) {
-      _locationSubscription.cancel();
+      _locationSubscription.cancel(); 
     }
     _locationSubscription = _locationTracker.onLocationChanged.listen((locationData) {
-      print(locationData);
-      distanceToGoal = Geolocator.distanceBetween(locationData.latitude, locationData.longitude, latitudeDouble, longitudeDouble);
+      var distanceToGoal = Geolocator.distanceBetween(locationData.latitude, locationData.longitude, latitudeDouble, longitudeDouble);
       print("Afstand tot doel: $distanceToGoal");
+    
+      if(distanceToGoal <= 2) {
+        print("uw product bevindt zich binnen een straal van 2 meter ");
+      }else {
+        print("nog eventjes zoeken");
+      }
+      // print(visible);
+      print("-----------------------------------");
+      
     });
   }
+
+  void _getCurrentLocation() async {
+    var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    // print(position);
+    latitudeDouble = double.parse(data['latitude']);
+    longitudeDouble = double.parse(data['longitude']);
+    print(latitudeDouble);
+    print(longitudeDouble);
+    
+    bearing = Geolocator.bearingBetween(position.latitude, position.longitude, latitudeDouble, longitudeDouble);
+    print(bearing);  
+    cords= position;
+    print(cords);
+    
+    _getDeclination(position.latitude, position.longitude, position.latitude).then((dec) {
+      setState(() {
+        _declination = dec;
+      });
+    });
+  }
+
 
   void _checkIfNull() {
     if(azimuth == null || direction == null || _declination == null) {
@@ -109,12 +109,10 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
   @override
   void initState() {
     super.initState();
-    setState(() {
       checkDeviceSensors();
       _checkIfNull();
       _getCurrentLocation();
       getLiveLocation();
-    });
   }
 
   Future<void> checkDeviceSensors() async {
@@ -149,6 +147,11 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
         DeviceOrientation.portraitUp,
@@ -166,17 +169,71 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
               // ),
               Align(
                 alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 50, left: 10),
-                  child: IconButton(
-                    icon: Icon(Icons.arrow_back,
-                      size: 30,
-                      color: Colors.white,  
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 50, left: 10),
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back,
+                          size: 30,
+                          color: Colors.white,  
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
+                    Container(
+                      child: StreamBuilder(
+                        stream: _locationTracker.onLocationChanged,
+                        builder: (BuildContext context, AsyncSnapshot locationShot){
+                          if(locationShot.hasData){
+
+                            var distanceStream = Geolocator.distanceBetween(locationShot.data.latitude, locationShot.data.longitude, latitudeDouble, longitudeDouble);
+                            var distanceStreamRounded = double.parse((distanceStream).toStringAsFixed(0));
+
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              alignment: Alignment.center,
+                              child: Container(
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.all(10),
+                                width: 100,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                  color: Colors.blueGrey[900],
+                                ), 
+                                child: Text(distanceStreamRounded.toString()+' meter', style: 
+                                  TextStyle(
+                                    color: Colors.white
+                                  ),
+                                )
+                              )
+                            );
+                          }else 
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            alignment: Alignment.center,
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(10),
+                              width: 200,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                color: Colors.blueGrey[900],
+                              ), 
+                              child: Text('Distance is being calculated', style: 
+                                TextStyle(
+                                  color: Colors.white
+                                ),
+                              )
+                            )
+                          );
+                        },
+                      )
+                    )
+                  ],
                 ),
               ),
               Align(
@@ -186,27 +243,24 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
                     stream: Compasstools.azimuthStream,
                     builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
                       if (snapshot.hasData) {
-                        // print(snapshot.data);
+
                       azimuth = _declination + snapshot.data;
                       direction = azimuth - bearing;
                       var directHeading = double.parse((direction).toStringAsFixed(12));
-                      // print(directHeading);
-                      
-                      // print(azimuth);
-                      // print(direction);
-                        return Padding(
-                          padding: EdgeInsets.all(20),
-                          child: Center(
-                              child: new RotationTransition(
-                                turns: new AlwaysStoppedAnimation(-directHeading/360),
-                                child: Object3D(
-                                  size: Size(10, 10),
-                                  path: "assets/file.obj",
-                                  asset: true,
-                                ),
-                              )
-                          ),
-                        );
+
+                      return Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Center(
+                            child: new RotationTransition(
+                              turns: new AlwaysStoppedAnimation(-directHeading/360),
+                              child: Object3D(
+                                size: Size(10, 10),
+                                path: "assets/file.obj",
+                                asset: true,
+                              ),
+                            )
+                        ),
+                      );
                     } 
                       return Text("Error in stream");
                     },
@@ -222,26 +276,13 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       Container(
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.centerRight,
-                        child: FloatingActionButton(
-                            backgroundColor: Colors.blue,
-                            child: Icon(Icons.refresh, 
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              _getCurrentLocation();
-                            }
-                          ),
-                      ),
-                      Container(
                         margin: EdgeInsets.only(top: 25),
                         height: 150,
                         width: MediaQuery.of(context).size.width,
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                          color: Colors.blueGrey[900],
+                          color: Colors.grey,
                         ),
                         child:Row(
                           children: [
@@ -262,7 +303,7 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
                     ],
                   ),
                 ),
-              )
+              ),
             ],
           )
         ),
@@ -281,30 +322,3 @@ class _ArCoreNavigatorState extends State<ArCoreNavigator> {
     
   }
 }
-
-// class  extends StatefulWidget {
-//   @override
-//   _State createState() => _State();
-// }
-
-// class _State extends State<> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return
-//       SplashScreen(
-//       seconds: 8,
-//       navigateAfterSeconds:
-      
-//       title: Text('Getting your location',
-//         style: TextStyle(
-//           fontWeight: FontWeight.bold,
-//           fontSize: 20.0
-//         ),
-//       ),
-//       backgroundColor: Colors.transparent,
-//       styleTextUnderTheLoader: new TextStyle(),
-//       onClick: ()=>print("Flutter Egypt"),
-//       loaderColor: Colors.red
-//     );
-//   }
-// }
